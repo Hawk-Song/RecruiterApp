0a2157235620e9fa35a895e63aab9e3108b39c15
hawk <savokisong@gmail.com>
2018-06-03 14:42:12 -0700

    implement login and register pages

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import Logo from '../../component/logo/logo'
+import {List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile'
 class Login extends React.Component {
-
+  constructor(props){
+    super(props)
+    //why not use arrow function
+    //arrow function will pasa new object every time
+    //bind this just old one, so a litter performance improvement
+    this.register = this.register.bind(this)
+  }
+  register(){
+    console.log(this.props)
+    this.props.history.push('/register')
+  }
   render(){
-    return <h2>Login Page</h2>
+    return (
+      <div>
+        <Logo></Logo>
+        <h2>Login Page</h2>
+        <WingBlank>
+          <List>
+            <InputItem>Username</InputItem>
+            <InputItem>Password</InputItem>
+          </List>
+          <WhiteSpace />
+          <Button type='primary'>Login</Button>
+          <WhiteSpace />
+          <Button onClick={this.register} type='primary'>Register</Button>
+        </WingBlank>
+      </div>
+    )
   }
 }
 
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Logo from '../../component/logo/logo'
+import {List, InputItem, Radio, WingBlank, WhiteSpace, Button} from 'antd-mobile'
+
 
 class Register extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      type:'genius'
+    }
+  }
 
   render(){
-    return <h2>Register Page</h2>
+    const RadioItem = Radio.RadioItem
+    return (
+      <div>
+        <Logo></Logo>
+        <h2>Register Page</h2>
+        <List>
+            <InputItem>Username</InputItem>
+            <InputItem>Password</InputItem>
+            <InputItem>Comfirm Password</InputItem>
+            <RadioItem checked={this.state.type==='genius'}>Genuis</RadioItem>
+            <RadioItem checked={this.state.type==='boss'}>Boss</RadioItem>
+            <Button type='primary'>Register</Button>
+        </List>
+      </div>
+    )
   }
 }
 
