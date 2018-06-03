9db0cc7a645d872e0994a747ce3ea517bf6de2c1
hawk <savokisong@gmail.com>
2018-06-03 11:36:53 -0700

    create login and register page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
+import Login from './container/login/login'
+import Register from './container/register/register'
 import reducers from './reducer'
 
 const store = createStore(reducers, compose(
@@ -15,6 +17,10 @@ const store = createStore(reducers, compose(
 ReactDOM.render(
   (<Provider store={store}>
     <BrowserRouter>
+      <div>
+        <Route path='/login' component={Login}></Route>
+        <Route path='/register' component={Register}></Route>
+      </div>
     </BrowserRouter>
    </Provider>), 
   document.getElementById('root')
