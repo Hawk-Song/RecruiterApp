a007539e5e959b29e051cba150d2b6650b9131d8
hawk <savokisong@gmail.com>
2018-06-03 17:36:41 -0700

    implement register interaction

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import axios from 'axios'
+import {withRouter} from 'react-router-dom'
 
+@withRouter
 class AuthRoute extends React.Component{
   componentDidMount(){
+    const publicList = ['/login', '/register']
+    const pathname = this.props.location.pathname
+    //if in login or register page, do not need to route
+    if (publicList.indexOf(pathname)>-1){
+      return null
+    }
     //get user information
     axios.get('/user/info').
       then(res=>{
         if (res.status===200){
+          if(res.data.code===0){
+            //if user loged in
+
+          }else{
+            //AuthRouter is not Router component,
+            //so if want use history, decorate it with withRouter
+            //if not at login or register, and user have not login in, route to login page
+            this.props.history.push('/login')
+          }
           console.log(res.data)
         }
       })
-    //is logined, user type etc
+    //is logined,
     //current url, have use complete their information
 
   }
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -7,8 +7,20 @@ class Register extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      user:'',
+      pwd:'',
+      repeatpwd:'',
       type:'genius'
     }
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+  handleChange(key, val){
+    this.setState({
+      [key]:val
+    })
+  }
+  handleRegister(){
+    console.log(this.state)
   }
 
   render(){
@@ -18,12 +30,16 @@ class Register extends React.Component {
         <Logo></Logo>
         <h2>Register Page</h2>
         <List>
-            <InputItem>Username</InputItem>
-            <InputItem>Password</InputItem>
-            <InputItem>Comfirm Password</InputItem>
-            <RadioItem checked={this.state.type==='genius'}>Genuis</RadioItem>
-            <RadioItem checked={this.state.type==='boss'}>Boss</RadioItem>
-            <Button type='primary'>Register</Button>
+            <InputItem onChange={v=>this.handleChange('user', v)}>Username</InputItem>
+            <InputItem onChange={v=>this.handleChange('pwd', v)}>Password</InputItem>
+            <InputItem onChange={v=>this.handleChange('repeatpwd', v)}>Cfm Pwd</InputItem>
+            <RadioItem checked={this.state.type==='genius'}
+              onChange={()=>this.handleChange('type', 'genius')}
+            >Genuis</RadioItem>
+            <RadioItem checked={this.state.type==='boss'}
+              onChange={()=>this.handleChange('type', 'boss')}
+            >Boss</RadioItem>
+            <Button type='primary' onClick={this.handleRegister}>Register</Button>
         </List>
       </div>
     )
