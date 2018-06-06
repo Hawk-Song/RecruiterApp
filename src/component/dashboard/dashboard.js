a5a82455644e95ede442c2b0ce1e3d516e188a3c
hawk <savokisong@gmail.com>
2018-06-06 15:21:52 -0700

    genius list content display

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {NavBar} from 'antd-mobile'
+import {Switch, Route} from 'react-router-dom'
 import NavLinkBar from '../navlink/navlink'
+import Boss from '../../component/boss/boss'
 
-function Boss(){
-  return <h2>Boss</h2>
-}
+// function Boss(){
+//   return <h2>Boss</h2>
+// }
 function Genius(){
   return <h2>Genius</h2>
 }
@@ -59,8 +61,14 @@ class Dashboard extends React.Component{
     ]
     return (
       <div>
-        <NavBar mode='dark'>{navList.find(v=>v.path===pathname).title}</NavBar>
-        <h2>content</h2>
+        <NavBar className='fixd-header' mode='dark'>{navList.find(v=>v.path===pathname).title}</NavBar>
+        <div style={{marginTop:45}}>
+          <Switch>
+            {navList.map(v=>(
+              <Route key={v.path} path={v.path} component={v.component}></Route>
+            ))}
+          </Switch>
+        </div>
         <NavLinkBar data ={navList}></NavLinkBar>
       </div>
 
