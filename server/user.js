c039e37905b6bcf461a89a66a23cec94a03002ae
hawk <savokisong@gmail.com>
2018-06-07 22:53:37 -0700

    chat head picture

--- server/user.js
+++ server/user.js
@@ -17,15 +17,21 @@ Router.get('/list', function(req, res){
 })
 
 Router.get('/getmsglist', function(req, res){
-  const user = req.cookies.user
+  const user = req.cookies.userid
   //Chat.remove({}, function(e,d){})
-  //find with multiple condition, use '$or'
-  //{'$or':[{from:user, to:user}]}
-  Chat.find({}, function(err, doc){
-    if(!err){
-      //console.log(doc)
-      return res.json({code:0, msgs:doc})
-    }
+
+  User.find({}, function(err, userdoc){
+    let users= {}
+    userdoc.forEach(v=>{
+      users[v._id] = {name:v.user, avatar:v.avatar}
+    })
+      //find with multiple condition, use '$or'
+    Chat.find({'$or':[{from:user}, {to:user}]}, function(err, doc){
+      if(!err){
+        return res.json({code:0, msgs:doc, users:users})
+      }
+    })
+    
   })
 })
 
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {List, InputItem, NavBar} from 'antd-mobile'
+import {List, InputItem, NavBar, Icon} from 'antd-mobile'
 import {connect} from 'react-redux'
 import {getMsgList, sendMsg, recvMsg} from '../../redux/chat.redux'
 import io from 'socket.io-client'
@@ -22,6 +22,10 @@ class Chat extends React.Component{
     //     msg:[...this.state.msg, data.text]
     //   })
     // })
+    if(!this.props.chat.chatmsg.length){
+      this.props.getMsgList()
+      this.props.recvMsg()
+    }
   }
   handleSubmit(){
     //socket.emit('sendmsg', {text:this.state.text})
@@ -35,23 +39,35 @@ class Chat extends React.Component{
     this.setState({text:''})
   }
   render(){
-    const user =this.props.match.params.user
+    const userid =this.props.match.params.user
     const Item = List.Item
+    const users = this.props.chat.users
+    if(!users[userid]){
+      return null
+    }
     return (
       <div id='chat-page'>
-        <NavBar mode='dark'>
-          {this.props.match.params.user}
+        <NavBar 
+          mode='dark' 
+          icon={<Icon type='left'/>}
+          onLeftClick={()=>{
+            this.props.history.goBack()
+          }}
+        >
+          {users[userid].name}
         </NavBar>
         {this.props.chat.chatmsg.map(v=>{
-          return v.from===user?(
+          const avatar = require(`../img/${users[v.from].avatar}.png`)
+          return v.from===userid?(
             <List key={v._id}>
               <Item
+                thumb={avatar}
               >{v.content}</Item>
             </List>
           ):(
             <List key={v._id}>
               <Item 
-                extra={'avatar'}
+                extra={<img src={avatar} alt=''/>}
                 className='chat-me'
               >{v.content}</Item>
             </List>
